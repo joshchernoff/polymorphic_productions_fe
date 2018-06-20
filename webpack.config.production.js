515dc5a1a5cd13c0bd9a6fb558a40e7838069cc7
Josh Chernoff <jchernoff@polymorphic.productions>
2018-06-19 18:53:14 -0700

    wip

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,21 +1,24 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 const webpackCommonConfig = require('./webpack.config.common');
 
 module.exports = merge(webpackCommonConfig, {
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			},
-			minimize: true,
-			sourceMap: true
-		}),
-		new webpack.EnvironmentPlugin({ NODE_ENV: 'production' }),
-	],
-	devtool: "source-map",
-	devServer: {
-		compress: true,
-	},
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true,
+        },
+        sourceMap: true,
+      }),
+    ],
+  },
 });
